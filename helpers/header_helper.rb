0c55e14e6d42e071360dd50246386f781741abe5
Lucas <contact@lucasvanbriemen.nl>
2026-05-31 19:08:15 +0200

    Allow for links.

--- helpers/header_helper.rb
+++ helpers/header_helper.rb
@@ -9,11 +9,11 @@ module HeaderHelper
     tag.header(class: "header") do
       image_tag("images/logo.svg", class: "logo") + content_tag(:p, title, class: "title") +
 
-      content_tag(:nav, class: "nav") do
-        items.each do |item|
-          concat link_to(item[:name], item[:href], class: "nav-item #{'active' if item[:active]}")
+        content_tag(:nav, class: "nav") do
+          items.each do |item|
+            concat link_to(item[:name], item[:href], class: "nav-item #{'active' if item[:active]}")
+          end
         end
-      end
     end
   end
 end
