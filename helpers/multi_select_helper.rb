825dcc278c6c18bcf3fa2281d7d5ed1b4ff955ac
Lucas <contact@lucasvanbriemen.nl>
2026-06-21 13:45:08 +0200

    Remove wrappers.

--- helpers/multi_select_helper.rb
+++ helpers/multi_select_helper.rb
@@ -49,17 +49,14 @@ module MultiSelectHelper
     value = option[:value].to_s
     label = option[:label]
 
-    tag.li(class: "multi-select__option",
-           data: { "multi-select-target": "option", label: label.to_s.downcase, name: label }) do
-      tag.label do
-        checkbox = form.check_box("#{name}[]", { id: "#{name}-#{value}", checked: selected.include?(value), data: { action: "multi-select#onToggle" } }, value, nil)
-        avatar = if option[:image].present?
-          tag.img(src: option[:image], class: "multi-select__avatar", loading: "lazy", alt: "")
-        else
-          "".html_safe
-        end
-        checkbox + avatar + tag.span(label)
+    tag.label(class: "multi-select__option", data: { "multi-select-target": "option", label: label.to_s.downcase, name: label }) do
+      checkbox = form.check_box("#{name}[]", { id: "#{name}-#{value}", checked: selected.include?(value), data: { action: "multi-select#onToggle" } }, value, nil)
+      avatar = if option[:image].present?
+        tag.img(src: option[:image], class: "multi-select__avatar", loading: "lazy", alt: "")
+      else
+        "".html_safe
       end
+      checkbox + avatar + tag.span(label)
     end
   end
 end
